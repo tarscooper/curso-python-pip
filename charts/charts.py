31ce5dfb26db978e817abc125d05ef2783428333
lincol gorpa <team@ncggroup.com>
2023-09-28 01:16:25 -0500

    edit values

--- charts/charts.py
+++ charts/charts.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 def generate_pie_chart():
     labels =['A','B','C']
-    values =[200,240,220]
+    values =[300,500,700]
 
     fig, ax = plt.subplots()
     ax.pie(values,labels=labels)
